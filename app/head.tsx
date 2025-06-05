f0e0847d99aa422f4ef97aca6745ee40d9827e4b
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2025-06-05 09:27:55 +0100

    added favicon

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+
+export default function Head() {
+  return (
+    <>
+      <title>Shop.co | Convenience E-commerce Store</title>
+      <meta
+        name="description"
+        content="Shop smarter with Shop.co — your go-to online convenience store for everyday essentials."
+      />
+      <link rel="icon" href="/favicon.ico"  />
+    </>
+  );
+}
